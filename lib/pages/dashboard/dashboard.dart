26f86ea589e7cda87fdaf6acb37d02cca90c6357
etienne <etiennewayne@gmail.com>
2024-11-24 22:56:25 +0800

    update dashboard

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -17,6 +17,26 @@ class Dashboard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Text(
+                  'Account No.',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  '202411335',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
                   'Current Balance',
                   textAlign: TextAlign.left,
                   style: TextStyle(
@@ -30,7 +50,38 @@ class Dashboard extends StatelessWidget {
                       fontSize: 40,
                       fontWeight: FontWeight.w700,
                       color: Colors.white),
-                )
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Card(
+          color: Colors.cyan,
+          elevation: 0,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(30, 30, 10, 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Previous Reading',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  '1002534',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
               ],
             ),
           ),
